534499e0f8d8cf4d4d1ba0593dbb468187d69164
Renan Andrade <fel9.renan02@gmail.com>
2023-03-13 16:30:41 -0300

    Changed footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserved</p>
+      <p>2023 Beat Store All rights reserved</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
